ee988be7116ecb50284e0931239c06fedb38ee4b
JuliaPoltavets` <juliapoltavec@gmail.com>
2019-09-10 05:31:17 -0700

    Routing implemented

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+// Ordering is IMPORTANT!!! resolved sequentially
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent }, // covers page http://localhost:4200
+    {
+        path: '', // dummy root which will result in concat parent path and child path
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+            { path: 'lists', component: ListsComponent },
+            { path: 'messages', component: MessagesComponent },
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
